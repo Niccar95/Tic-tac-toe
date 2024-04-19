57231dc77711c93256f96f93ae580911c4950afa
Niccar95 <niccar.95@outlook.com>
2024-04-19 18:03:18 +0200

    counter does not increase if property filled is set to true

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,22 +2,32 @@ import "../scss/style.scss";
 import { Gridbox } from "./models/Gridbox";
 
 const board = document.getElementById("board");
+
 let count: number = 0;
+
+let list: Gridbox[] = [];
 
 for (let i = 1; i < 10; i++) {
   const gridBox = document.createElement("section");
-  let GridBox = new Gridbox(i, gridBox, false);
+  let newGridBox = new Gridbox(i, gridBox, false);
+  list.push(newGridBox);
+
   gridBox.classList.add("gridBox");
   board?.appendChild(gridBox);
 
-  gridBox.innerHTML = GridBox.id.toString();
+  gridBox.innerHTML = newGridBox.id.toString();
 
   gridBox.addEventListener("click", () => {
+    if (newGridBox.filled) {
+      return;
+    }
+
     gridBox.innerHTML = "You clicked me";
+
+    newGridBox.filled = true;
+
     count++;
     console.log(count);
-
-    GridBox.filled = true;
 
     if (count % 2 == 0) {
       console.log("hello");
